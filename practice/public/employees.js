2db0dfcb267fdd101ec40cffbedf3942f1ac1b27
sanjanasujay <sanjana121123@gmail.com>
2026-04-21 22:26:22 -0700

    Add name and configure properties in package

--- practice/public/employees.js
+++ practice/public/employees.js
@@ -1,8 +1,10 @@
 let name1 = 'Smita';
 let name2 = 'Sanjana';
+let name3 = 'Robin';
 const element2 = /*#__PURE__*/React.createElement("ul", {
   style: {
     'color': 'blue',
     'fontSize': '24px'
   }
-}, /*#__PURE__*/React.createElement("li", null, name1), /*#__PURE__*/React.createElement("li", null, name2.toUpperCase));
+}, /*#__PURE__*/React.createElement("li", null, name1), /*#__PURE__*/React.createElement("li", null, name2.toUpperCase()), /*#__PURE__*/React.createElement("li", null, name3));
+ReactDOM.render(element2, document.getElementById('content'));
--- practice/src/employees.jsx
+++ practice/src/employees.jsx
@@ -1,8 +1,10 @@
 let name1 = 'Smita';
-        let name2 = 'Sanjana';
+let name2 = 'Sanjana';
+let name3 = 'Robin'
         const element2 = (
             <ul style={{'color': 'blue', 'fontSize': '24px'}}>
                 <li>{name1}</li>
-                <li>{name2.toUpperCase}</li>
+                <li>{name2.toUpperCase()}</li>
+                <li>{name3}</li>
                 </ul>
             )
