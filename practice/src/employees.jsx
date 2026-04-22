445ed92d58b723b76d3344fbb10534c1f9025de4
sanjanasujay <sanjana121123@gmail.com>
2026-04-21 18:41:23 -0700

    Install Babel

---
+++ practice/src/employees.jsx
@@ -0,0 +1,8 @@
+let name1 = 'Smita';
+        let name2 = 'Sanjana';
+        const element2 = (
+            <ul style={{'color': 'blue', 'fontSize': '24px'}}>
+                <li>{name1}</li>
+                <li>{name2.toUpperCase}</li>
+                </ul>
+            )
