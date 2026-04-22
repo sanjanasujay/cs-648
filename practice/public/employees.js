63eb15aef9a837b32973d3ef508923c3c2cd88d2
sanjanasujay <sanjana121123@gmail.com>
2026-04-21 18:48:38 -0700

    Compile JSX using Babel to a specific directory

---
+++ practice/public/employees.js
@@ -0,0 +1,8 @@
+let name1 = 'Smita';
+let name2 = 'Sanjana';
+const element2 = /*#__PURE__*/React.createElement("ul", {
+  style: {
+    'color': 'blue',
+    'fontSize': '24px'
+  }
+}, /*#__PURE__*/React.createElement("li", null, name1), /*#__PURE__*/React.createElement("li", null, name2.toUpperCase));
