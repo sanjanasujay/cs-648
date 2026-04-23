c1053cf82440d256afb7039965777acdb8b3d76d
sanjanasujay <sanjana121123@gmail.com>
2026-04-22 17:54:49 -0700

    Within app.js, add the necessary code to: import express (make sure to add the 'type': 'module' property and value set within the package.json file. create the express app set the 'public' folder as a static folder get the '/' route and have it load 'index.html' from the public folder have the app listen on port 5000 Now, move the index.html file into the 'public' folder. You should have two files in this folder now: the index.html file and the employees.js file which is automatically created by Babel from the employees.jsx file

--- practice/app.js
+++ practice/app.js
@@ -1,4 +1,13 @@
-const express = require('express');
-const app = express();
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+
+app.listen(5000, () => console.log('Server running on port 5000'));
