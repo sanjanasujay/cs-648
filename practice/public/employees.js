c79c8dc84e62c3b888c4dc09287fc167be9448d7
sanjanasujay <sanjana121123@gmail.com>
2026-04-22 18:06:51 -0700

    Troubleshoot 403 error install missing dependencies

--- practice/public/employees.js
+++ practice/public/employees.js
@@ -1,10 +1,11 @@
 let name1 = 'Smita';
 let name2 = 'Sanjana';
 let name3 = 'Robin';
+let name4 = 'Jordan';
 const element2 = /*#__PURE__*/React.createElement("ul", {
   style: {
     'color': 'blue',
     'fontSize': '24px'
   }
-}, /*#__PURE__*/React.createElement("li", null, name1), /*#__PURE__*/React.createElement("li", null, name2.toUpperCase()), /*#__PURE__*/React.createElement("li", null, name3));
+}, /*#__PURE__*/React.createElement("li", null, name1), /*#__PURE__*/React.createElement("li", null, name2.toUpperCase()), /*#__PURE__*/React.createElement("li", null, name3), /*#__PURE__*/React.createElement("li", null, name4));
 ReactDOM.render(element2, document.getElementById('content'));
--- practice/src/employees.jsx
+++ practice/src/employees.jsx
@@ -1,10 +1,15 @@
 let name1 = 'Smita';
 let name2 = 'Sanjana';
-let name3 = 'Robin'
-        const element2 = (
-            <ul style={{'color': 'blue', 'fontSize': '24px'}}>
-                <li>{name1}</li>
-                <li>{name2.toUpperCase()}</li>
-                <li>{name3}</li>
-                </ul>
-            )
+let name3 = 'Robin';
+let name4 = 'Jordan';
+
+const element2 = (
+    <ul style={{'color': 'blue', 'fontSize': '24px'}}>
+        <li>{name1}</li>
+        <li>{name2.toUpperCase()}</li>
+        <li>{name3}</li>
+        <li>{name4}</li>
+    </ul>
+);
+
+ReactDOM.render(element2, document.getElementById('content'));
